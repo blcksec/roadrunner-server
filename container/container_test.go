fceda8617323cd1e477bc9f475f484d4577527df
root <root@blckswan>
2022-10-28 08:30:34 +0200

    .

---
+++ container/container_test.go
@@ -0,0 +1,25 @@
+package container_test
+
+import (
+	"testing"
+	"time"
+
+	endure "github.com/roadrunner-server/endure/pkg/container"
+	"github.com/blcksec/roadrunner/v2/container"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewContainer(t *testing.T) { // there is no legal way to test container options
+	c, err := container.NewContainer(container.Config{})
+	c2, err2 := container.NewContainer(container.Config{
+		GracePeriod: time.Second,
+		PrintGraph:  true,
+		LogLevel:    endure.WarnLevel,
+	})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, c)
+
+	assert.NoError(t, err2)
+	assert.NotNil(t, c2)
+}
---
+++ internal/cli/reset/command.go
@@ -0,0 +1,74 @@
+package reset
+
+import (
+	"log"
+	"sync"
+
+	internalRpc "github.com/blcksec/roadrunner/v2/internal/rpc"
+
+	"github.com/roadrunner-server/errors"
+	"github.com/spf13/cobra"
+)
+
+const (
+	op            = errors.Op("reset_handler")
+	resetterList  = "resetter.List"
+	resetterReset = "resetter.Reset"
+)
+
+// NewCommand creates `reset` command.
+func NewCommand(cfgFile *string, override *[]string, silent *bool) *cobra.Command {
+	return &cobra.Command{
+		Use:   "reset",
+		Short: "Reset workers of all or specific RoadRunner service",
+		RunE: func(_ *cobra.Command, args []string) error {
+			if cfgFile == nil {
+				return errors.E(op, errors.Str("no configuration file provided"))
+			}
+
+			client, err := internalRpc.NewClient(*cfgFile, *override)
+			if err != nil {
+				return err
+			}
+
+			defer func() { _ = client.Close() }()
+
+			plugins := args        // by default we expect services list from user
+			if len(plugins) == 0 { // but if nothing was passed - request all services list
+				if err = client.Call(resetterList, true, &plugins); err != nil {
+					return err
+				}
+			}
+
+			var wg sync.WaitGroup
+			wg.Add(len(plugins))
+
+			for _, plugin := range plugins {
+				// simulating some work
+				go func(p string) {
+					if !*silent {
+						log.Printf("resetting plugin: [%s] ", p)
+					}
+					defer wg.Done()
+
+					var done bool
+					<-client.Go(resetterReset, p, &done, nil).Done
+
+					if err != nil {
+						log.Println(err)
+
+						return
+					}
+
+					if !*silent {
+						log.Printf("plugin reset: [%s]", p)
+					}
+				}(plugin)
+			}
+
+			wg.Wait()
+
+			return nil
+		},
+	}
+}
